e54ce64f8f666d37b9a67297f9b8d867769b81ff
RancidEnsign95 <93900701+RancidEnsign95@users.noreply.github.com>
2021-11-28 22:16:51 +0530

    Add files via upload

---
+++ prime numbers.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{ 
+int a;
+printf("enter the number");
+scanf("%d",a);
+if (a%2==1)
+printf("%d is a Prime number",a);
+else
+printf("%d number is composite",a);
+return 0;
+}
