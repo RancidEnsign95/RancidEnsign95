8e8bf8bfbb638811b142af4aa288566844365a22
RancidEnsign95 <93900701+RancidEnsign95@users.noreply.github.com>
2021-11-28 22:15:55 +0530

    Add files via upload

---
+++ even or odd.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{ int a;
+printf("enter the number");
+scanf("%d",&a);
+if (a%2==0)
+printf("the number is even %d",a);
+else
+printf("the number is odd %d",a);
+return 0;
+}
